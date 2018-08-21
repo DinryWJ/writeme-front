4732c883f03d29e274e25bac7e0b644dbdbac18a
fmuser <1059764284@qq.com>
2018-08-21 10:52:42 +0800

    settin
    setting

--- src/views/my/setting.vue
+++ src/views/my/setting.vue
@@ -1,20 +1,26 @@
 <template>
-  <el-tabs :tab-position="tabPosition" style="height: 600px;">
+  <el-tabs :tab-position="tabPosition" style="height: 800px">
      
     <el-tab-pane label="账号与密码">
+      
         <h2>账号与密码</h2>
         <hr/>
-        <div class="demo-input-suffix">
-          <span>密码</span><el-input style="margin-left:50px" placeholder="请输入内容" v-model="pass" clearable></el-input>
+        <h3>密码</h3>
+        <div class="password">
+          <el-input style="margin-left:50px;margin-bottom:20px" placeholder="请输入新密码" v-model="pass" clearable></el-input>
+          <el-input style="margin-left:50px" placeholder="请重新输入新密码" v-model="pass" clearable></el-input>
         </div>
         <hr class="hr0" />  
-        <div class="demo-input-suffix">
-          手机<el-input style="margin-left:50px" placeholder="请输入内容" v-model="phone" clearable></el-input>
+        <h3>手机</h3>
+        <div class="phone">
+          <el-input style="margin-left:50px" placeholder="请输入内容" v-model="phone" clearable></el-input>
         </div>
          <hr class="hr0" />  
-         <div class="demo-input-suffix">
-          个性签名<el-input style="margin-left:20px" placeholder="请输入内容" v-model="sign" clearable></el-input>
+         <h3>个性签名</h3>
+         <div class="sign">
+          <el-input style="margin-left:50px" type="textarea" placeholder="请输入内容" v-model="desc" clearable></el-input>
         </div>
+        <el-button style="margin-top:30px" type="primary" round>保存</el-button>
     </el-tab-pane>
     <el-tab-pane type="border-card" label="消息设置">
         <h2>消息设置</h2>
@@ -26,6 +32,8 @@
           <el-switch
           style="margin-top: 35px;"
             v-model="value1"
+            active-text="ON"
+            inactive-text="OFF"
             active-color="#13ce66"
             inactive-color="#ff4949">
           </el-switch>
@@ -36,6 +44,8 @@
           <el-switch
           style="margin-top: 35px;"
             v-model="value2"
+            active-text="ON"
+            inactive-text="OFF"
             active-color="#13ce66"
             inactive-color="#ff4949">
           </el-switch>
@@ -49,6 +59,8 @@
          <span>在站外隐藏个人信息</span>
           <el-switch
             v-model="value3"
+            active-text="ON"
+            inactive-text="OFF"
             active-color="#13ce66"
             inactive-color="#ff4949">
           </el-switch>
@@ -60,11 +72,16 @@
         <span>在我上传的图片上显示水印</span>
           <el-switch
             v-model="value4"
+            active-text="ON"
+            inactive-text="OFF"
             active-color="#13ce66"
             inactive-color="#ff4949">
           </el-switch>
     </el-tab-pane>
+
+     
   </el-tabs>
+  
 </template>
 
 <style scoped>
@@ -81,10 +98,22 @@
     border:none;
     border-top:1px dashed rgb(67, 72, 77);
     } 
-    .demo-input-suffix{
+    .phone{
       height: 70px;
-      line-height: 70px;
+      
     }
+    .password{
+       height: 120px;
+      
+    }
+    .sign{
+      height: 100px;
+      width: 50%;
+    }
+ .demo-input-suffix{
+height: 70px;
+line-height: 70px;
+ }
 </style>
 
 
@@ -94,7 +123,10 @@
       return {
         tabPosition: 'left',
         pass:'',
-
+        value1:true,
+        value2:true,
+         value3:true,
+          value4:true,
       };
     }
   };
